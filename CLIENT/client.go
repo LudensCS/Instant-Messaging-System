9589ca64f956a870fd00b670be055ecf2012e39a
pecksniffk <1785932641@qq.com>
2025-07-09 21:52:02 +0800

    客户端菜单功能实现以及业务方法初步搭建

--- CLIENT/client.go
+++ CLIENT/client.go
@@ -12,6 +12,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	Conn       net.Conn
+	flag       int //用户模式
 }
 
 // 创建一个Client接口
@@ -25,10 +26,47 @@ func NewClient(serverIp string, serverPort int) *Client {
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
 		Conn:       conn,
+		flag:       1,
 	}
 	return client
 }
-func (this *Client) Handler() {
+
+// 客户端菜单
+func (this *Client) Menu() bool {
+	var flag int
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户名")
+	fmt.Println("0.退出")
+	fmt.Scan(&flag)
+	if flag >= 0 && flag <= 3 {
+		this.flag = flag
+		return true
+	} else {
+		fmt.Println("<<<<<<<<请输入合法范围内的数字>>>>>>>>")
+		return false
+	}
+}
+
+// 客户端业务
+func (this *Client) Run() {
+	for {
+		for {
+			if this.Menu() {
+				break
+			}
+		}
+		switch this.flag {
+		case 0:
+			return
+		case 1:
+			fmt.Println("公聊模式...")
+		case 2:
+			fmt.Println("私聊模式...")
+		case 3:
+			fmt.Println("更新用户名...")
+		}
+	}
 }
 
 var serverIp string
@@ -48,7 +86,6 @@ func main() {
 		return
 	}
 	fmt.Println("<<<<<<<<链接服务器成功>>>>>>>>")
-	go client.Handler()
-	for {
-	}
+	//启动客户端业务
+	client.Run()
 }
