9e351b42385cba1c308ad51c5efc680057df11e0
pecksniffk <1785932641@qq.com>
2025-07-10 11:03:24 +0800

    客户端更改用户名接口实现以及监听服务端回应功能实现

--- CLIENT/client.go
+++ CLIENT/client.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 // 客户端
@@ -26,19 +29,19 @@ func NewClient(serverIp string, serverPort int) *Client {
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
 		Conn:       conn,
-		flag:       1,
+		flag:       4869,
 	}
 	return client
 }
 
 // 客户端菜单
 func (this *Client) Menu() bool {
-	var flag int
+	var flag int = 4869
 	fmt.Println("1.公聊模式")
 	fmt.Println("2.私聊模式")
 	fmt.Println("3.更新用户名")
 	fmt.Println("0.退出")
-	fmt.Scan(&flag)
+	fmt.Scanln(&flag)
 	if flag >= 0 && flag <= 3 {
 		this.flag = flag
 		return true
@@ -46,6 +49,32 @@ func (this *Client) Menu() bool {
 		fmt.Println("<<<<<<<<请输入合法范围内的数字>>>>>>>>")
 		return false
 	}
+}
+
+// 更新用户名
+func (this *Client) UpdateName() bool {
+	fmt.Println("<<<<<<<<请输入您的新用户名>>>>>>>>")
+	//读取包含空格的字符串方法
+	reader := bufio.NewReader(os.Stdin)
+	msg, ERR := reader.ReadString('\n')
+	if ERR != nil {
+		fmt.Println("reader.ReadString error:", ERR)
+		return false
+	}
+	this.Name = msg
+	msg = "更改用户名@群助手@" + this.Name
+	_, err := this.Conn.Write([]byte(msg))
+	if err != nil {
+		fmt.Println("Conn.Write error:", err)
+		return false
+	}
+	return true
+}
+
+// 打印服务端的回应消息,读写分离的思路
+func (this *Client) DoResponse() {
+	//这行代码相当于输出重定向,一旦连接内有数据就立刻输出,跟自己实现缓冲区读取后输出是等价的
+	io.Copy(os.Stdout, this.Conn)
 }
 
 // 客户端业务
@@ -64,7 +93,8 @@ func (this *Client) Run() {
 		case 2:
 			fmt.Println("私聊模式...")
 		case 3:
-			fmt.Println("更新用户名...")
+			for this.UpdateName() == false {
+			}
 		}
 	}
 }
@@ -86,6 +116,7 @@ func main() {
 		return
 	}
 	fmt.Println("<<<<<<<<链接服务器成功>>>>>>>>")
+	go client.DoResponse()
 	//启动客户端业务
 	client.Run()
 }
